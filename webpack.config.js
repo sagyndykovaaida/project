0f786831f0ab7cffb0df74416f7beffe3127308d
SagyndykovaA <SagyndykovaA@kacd.kz>
2023-07-03 19:10:38 +0600

    wrong commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,56 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, 'dist'), 
+//     },
+//     compress: true, 
+//     port: 3000, 
+//     open: {
+//       target: 'http://localhost:3000/', 
+//     },
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html',
+//     }),
+//   ],
+// };
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './angular-app/src/main.ts', // Update the entry point
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'), 
+      directory: path.join(__dirname, 'dist'),
     },
-    compress: true, 
-    port: 3000, 
+    compress: true,
+    port: 3000,
     open: {
-      target: 'http://localhost:3000/', 
+      target: 'http://localhost:3000/',
     },
   },
   module: {
@@ -24,11 +59,19 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.ts$/, // Add a rule to handle TypeScript files
+        use: ['ts-loader'],
+        exclude: /node_modules/,
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'], // Add TypeScript extension
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './angular-app/src/index.html', // Update the template path
     }),
   ],
 };
